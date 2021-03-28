b10895d12f739856eef942d214a7328b58040a38
lalith <lalluyagnavlkya.t@gmail.com>
2021-03-28 13:55:21 +0530

    add environmental vars

---
+++ app.js
@@ -0,0 +1,85 @@
+require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const userSchema = new mongoose.Schema ({
+    email: String,
+    password: String
+});
+
+
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+
+const User = new mongoose.model("user", userSchema);
+
+//////////////////////////////////home page////////////////////////////////
+app.route("/")
+.get(function(req, res){
+    res.render("home");
+});
+
+///////////////////////////////login page//////////////////////////////////
+app.route("/login")
+.get(function(req, res){
+    res.render("login");
+})
+.post(function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function(err, foundUser){
+        if(!err){
+            if(foundUser){
+                if(foundUser.password === password){
+                    res.render("secrets");
+                }
+            }
+        }else{
+            console.log(err);
+        }
+    })
+});
+/////////////////////register page/////////////////////////////////
+app.route("/register")
+.get(function(req, res){
+    res.render("register");
+})
+.post(function(req, res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save(function(err){
+        if(!err){
+            console.log("New user registered username:" + newUser.email);
+            res.render("secrets");
+        }else{
+            console.log(err);
+        }
+    });
+});
+
+app.route("submit")
+.post(function(req, res){
+    
+});
+
+
+
+
+app.listen(3000, function(){
+    console.log("The sever is hosted on port 3000");
+})
